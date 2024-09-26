761522ad95f2c55d8ebb2d0ffa1d8b7689b440cf
Jaguso <jagutzo@gmail.com>
2024-09-25 21:07:16 -0600

    test 30

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Worldas!'
 
-
 # needed to add host to listen in localhost
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
